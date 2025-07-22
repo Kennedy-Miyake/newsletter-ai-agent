3df3741f124c8b8a96457f13a7460c05b3a1a087
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-07-22 01:00:09 -0300

    feat: created the OpenRouter class
    added the openrouter key

--- openrouter-connection.py
+++ openrouter-connection.py
@@ -5,3 +5,11 @@ from pydantic import (Field, SecretStr)
 from langchain import hub
 from langchain_core.utils.utils import secret_from_env
 from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+class OpenRouter(ChatOpenAI):
+    openai_api_key: Optional[SecretStr] = Field(
+        alias="api_key",
+        default_factory=secret_from_env("OPENROUTER_API_KEY", default=None)
+    )
