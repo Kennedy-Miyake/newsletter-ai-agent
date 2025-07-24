178d320a4cdefb72349d5b43931ee841ad160fc4
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-07-24 14:44:23 -0300

    feat: extracting the category, titles, author and date of publication of the news items

--- src/utils/webscraping.py
+++ src/utils/webscraping.py
@@ -9,5 +9,14 @@ class Scraping:
         return BeautifulSoup(self.raw_content, 'html.parser')
 
     def techcrunch(self):
-        raw_notices = self.parser_content.find_all('ul', class_='wp-block-post-template')
-        print(raw_notices)
+        notices = []
+        
+        raw_notices = self.parser_content.find_all('li', class_='wp-block-post', limit=30)
+        for raw_notice in raw_notices:
+            category = raw_notice.find('div', class_='loop-card__cat-group').get_text(strip=True)
+            title = raw_notice.find('h3', class_='loop-card__title').get_text(strip=True)
+            author = raw_notice.find('ul', class_='loop-card__author-list').get_text(strip=True)
+            time = raw_notice.find('time', class_='loop-card__time').get_text(strip=True)
+            notices.append(dict({'Category': category, 'Title': title, 'Author': author, 'Time': time})) 
+
+        return notices
