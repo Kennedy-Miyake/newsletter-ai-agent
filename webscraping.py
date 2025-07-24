e31892951ab3484061774c7168c9ebadf43a3164
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-07-24 11:59:35 -0300

    mod: added html parsing attribute

--- webscraping.py
+++ webscraping.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 class scraping:
     def __init__(self, content):
         self.raw_content = content
+        self.parser_content = self.get_html_parser()
 
     def get_html_parser(self):
         return BeautifulSoup(self.raw_content, 'html.parser')
