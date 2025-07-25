4dc26cba8e61c85fba9e616737f2fce8eef4ecfb
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-07-25 00:56:57 -0300

    refactor: renamed the function get_techcrunch_notices to fetch_techcrunch_notices
    changed the structure of notices.append
    
    function returns the news

--- src/utils/webscraping.py
+++ src/utils/webscraping.py
@@ -30,7 +30,7 @@ class ScrapingNews:
             
             return content
     
-    async def __get_techcrunch_notices(self):
+    async def __fetch_techcrunch_notices(self):
         notices = []
         
         notices_content = await self.__get_content(self.__website_url)
@@ -41,9 +41,14 @@ class ScrapingNews:
             title = raw_notice.find('h3', class_='loop-card__title').get_text(strip=True)
             author = raw_notice.find('ul', class_='loop-card__author-list').get_text(strip=True)
             time = raw_notice.find('time', class_='loop-card__time').get_text(strip=True)
-            notices.append(dict({'Category': category, 'Title': title, 'Author': author, 'Time': time})) 
+            notices.append(dict({
+                'Category': category,
+                'Title': title,
+                'Author': author,
+                'Time': time
+            })) 
 
-        self.__notices = notices
+        return notices
 
     def get_website_url(self):
         return self.__website_url
