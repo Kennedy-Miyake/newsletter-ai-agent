833b68eb8bc7a9e99f41cb0e2d381e64db9dddb4
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-07-24 22:38:57 -0300

    refactor: rename class Scraping to ScrapingNews

--- src/utils/webscraping.py
+++ src/utils/webscraping.py
@@ -1,21 +1,12 @@
 from bs4 import BeautifulSoup
 
-class Scraping:
+class ScrapingNews:
 
-    __raw_content = None
-    __parser_content = None
     
-    def __init__(self, content):
-        self.__raw_content = content
-        self.__parser_content = self.__get_html_parser()
 
-    def __get_html_parser(self):
-        return BeautifulSoup(self.__raw_content, 'html.parser')
 
-    def get_techcrunch_notices(self):
         notices = []
         
-        raw_notices = self.__parser_content.find_all('li', class_='wp-block-post', limit=30)
         for raw_notice in raw_notices:
             category = raw_notice.find('div', class_='loop-card__cat-group').get_text(strip=True)
             title = raw_notice.find('h3', class_='loop-card__title').get_text(strip=True)
