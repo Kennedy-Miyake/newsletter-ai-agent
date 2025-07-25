57f49ea17aa9664e8ab42d3b3bcae1e2bbd33644
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-07-25 01:14:00 -0300

    refactor: renamed all occurrences of notices to news

--- src/utils/webscraping.py
+++ src/utils/webscraping.py
@@ -4,7 +4,7 @@ from playwright.async_api import async_playwright
 class ScrapingNews:
 
     __website_url = None
-    __notices = []
+    __news = []
 
     def __init__(self, website_name):
         self.__website_url = self.__get_website_url(website_name)
@@ -30,47 +30,47 @@ class ScrapingNews:
             
             return content
     
-    async def select_notice(self):
-        selected_notice = dict()
+    async def select_news(self):
+        selected_news = dict()
 
-        self.__notices = await self.__fetch_techcrunch_notices()
+        self.__news = await self.__fetch_techcrunch_news()
 
         i = 0
-        for notice in self.__notices:
-            (category, title, author, time) = notice.values()
+        for news in self.__news:
+            (category, title, author, time) = news.values()
             print(f'{i} | {category} | {title}')
             print(f'{author} | {time}\n')
             i += 1
 
-        select_notice = int(input(f'Qual notícia você deseja selecionar (0-{(len(self.__notices))-1})? R: '))
-        if (select_notice >= 0) and (select_notice <= len(self.__notices)):
-            selected_notice = self.__notices[select_notice]
-            return selected_notice
+        select_news = int(input(f'Qual notícia você deseja selecionar (0-{(len(self.__news))-1})? R: '))
+        if (select_news >= 0) and (select_news <= len(self.__news)):
+            selected_news = self.__news[select_news]
+            return selected_news
 
         print(f'Notícia não encontrada!')
         
-    async def __fetch_techcrunch_notices(self):
-        notices = []
+    async def __fetch_techcrunch_news(self):
+        news = []
         
-        notices_content = await self.__get_content(self.__website_url)
-        notices_parser = BeautifulSoup(notices_content, 'html.parser')
-        raw_notices = notices_parser.find_all('li', class_='wp-block-post', limit=30)
-        for raw_notice in raw_notices:
-            category = raw_notice.find('div', class_='loop-card__cat-group').get_text(strip=True)
-            title = raw_notice.find('h3', class_='loop-card__title').get_text(strip=True)
-            author = raw_notice.find('ul', class_='loop-card__author-list').get_text(strip=True)
-            time = raw_notice.find('time', class_='loop-card__time').get_text(strip=True)
-            notices.append(dict({
+        news_content = await self.__get_content(self.__website_url)
+        news_parser = BeautifulSoup(news_content, 'html.parser')
+        raw_news = news_parser.find_all('li', class_='wp-block-post', limit=30)
+        for raw_news in raw_news:
+            category = raw_news.find('div', class_='loop-card__cat-group').get_text(strip=True)
+            title = raw_news.find('h3', class_='loop-card__title').get_text(strip=True)
+            author = raw_news.find('ul', class_='loop-card__author-list').get_text(strip=True)
+            time = raw_news.find('time', class_='loop-card__time').get_text(strip=True)
+            news.append(dict({
                 'Category': category,
                 'Title': title,
                 'Author': author,
                 'Time': time
             })) 
 
-        return notices
+        return news
 
     def get_website_url(self):
         return self.__website_url
 
-    def get_notices(self):
-        return self.__notices
+    def get_news(self):
+        return self.__news
