a9b1dec3cb683da7356ad5373bf471828da84661
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-07-27 03:57:52 -0300

    feat: added the method to get all the raw HTML

--- src/scraping_strategies/scraping_strategy.py
+++ src/scraping_strategies/scraping_strategy.py
@@ -1,9 +1,10 @@
 from abc import (ABC, abstractmethod)
 
 class ScrapingStrategy(ABC):
+    async def _get_raw_content(self, page):
+        return await page.content()
+
     @abstractmethod
-    def scrape_all_news(self):
         pass
 
-    def scrape_news_content(self):
         pass
