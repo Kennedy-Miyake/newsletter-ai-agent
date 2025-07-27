01910dfc222aef031a0cddc9990819277f6289ef
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-07-27 05:23:22 -0300

    feat: created the package for the TechCrunchScrapingStrategy class

---
+++ src/scraping_strategies/techcrunch_strategy.py
@@ -0,0 +1,8 @@
+from src.scraping_strategies.scraping_strategy import ScrapingStrategy
+
+class TechCrunchScrapingStrategy(ScrapingStrategy):
+    def scrape_all_news(self, parser):
+        pass
+
+    def scrape_news_content(self):
+        pass
