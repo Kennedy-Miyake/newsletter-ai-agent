5f1a194356c6a99c1c831d8f2f4de041b63ee540
Kennedy Miyake <kennedykaiak57@gmail.com>
2025-07-29 23:32:22 -0300

    feat: added an attribute with the default prompt template

--- src/utils/summarizer.py
+++ src/utils/summarizer.py
@@ -1,6 +1,22 @@
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 class Summarizer:
 
-    def __init__(self):
-        pass
+    __PROMPT_TEMPLATE = ChatPromptTemplate.from_messages([
+        ("system", """
+            [RESUME CONFIG]:
+            - Resumo com uma linguagem informativa para todo o público de TI.
+            - O resumo deve conter todo o **CONTEÚDO CHAVE** do artigo da notícia.
+            - Utilize de trocadilhos engraçados da área de TI (quando necessário, sem exageros).
+            - O resumo deve conter pelo menos 1/4 (um quarto) do artigo.
+            [INSTRUCTION]:
+            1. Faça uma análise do artigo em [ARTICLE].
+            2. Entenda todo o contexto.
+            3. Elabore um resumo baseado na [RESUME CONFIG].
+            """),
+        ("human", """
+            [ARTICLE]:
+            {input}
+            """)
+        ])
